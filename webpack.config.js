29d208005cdd211b27d763474dc0716f26c430a0
Joao Netto <jnetto@fyyb.com.br>
2019-10-16 23:31:44 -0300

    update webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,47 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry: ["@babel/polyfill", "./src/main.js"],
     output: {
-        path: path.resolve(__dirname ,'public'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname ,'dist'),
+        filename: 'assets/js/[name].bundle.js',
     },
     devServer: {
-        contentBase: __dirname + '/public'
+        contentBase: __dirname + '/dist'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
                 use: {
                     loader: 'babel-loader'
                 }
             }
         ],
     },
-
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: 'assets/css/[name].css',
+        //   chunkFilename: '[id].css',
+          ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+      ]
 };
